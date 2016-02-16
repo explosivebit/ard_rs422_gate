b123de3549233f65b939a3a999a23a5d454d0686
explosivebit <explosivebit@gmail.com>
2016-02-16 17:21:45 +0300

    ard_rs422_gate.ino

---
+++ ard_rs422_gate/ard_rs422_gate.ino
@@ -0,0 +1,33 @@
+include <SoftwareSerial.h>
+
+#define SSerialRX        10
+#define SSerialTX        11 
+ 
+SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
+
+const int ledPin =  12;     // TX
+const int ledPin2 =  9;     // RX
+int lectura = 100;
+
+void setup() { 
+  Serial.begin(9600);  
+  pinMode(ledPin, OUTPUT);
+  RS485Serial.begin(9600);
+  digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin, LOW);
+} 
+ 
+void loop(){
+  digitalWrite(ledPin, HIGH);
+  RS485Serial.write(lectura);
+  Serial.println(lectura);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  if(RS485Serial.available()){ 
+    digitalWrite(ledPin2, HIGH); 
+    int funcion = RS485Serial.read();
+    Serial.println(funcion);
+  }else{
+      digitalWrite(ledPin2, LOW);
+    }
+}
